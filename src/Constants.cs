ed800e915d4cbcab97303fa2a7aa29a9a0b96bd2
st3phhays <steph@stephhays.dev>
2022-07-23 20:55:25 -0500

    Move Common Items Into Toppings
    This moves shared assets into the Toppings repository so that they can
    be utilized by every CRAFTASTACK project.

---
+++ src/Constants.cs
@@ -0,0 +1,14 @@
+namespace Craftastack
+{
+    public static class Constants
+    {
+        public static class Site
+        {
+            public const string Title = "CRAFTASTACK";
+            public const string Tagline = "Craft your next tech stack";
+            public const string Url = "craftastack.io";
+            public const string Excerpt = "Craft your next tech stack.";
+            public const string Brand = "craftastack";
+        }
+    }
+}
